f8b2af430305ea9efa0752eada5f9b3baede659e
Anirudh Ramakrishnan <38531113+anirudh-r5@users.noreply.github.com>
2025-04-08 02:07:29 -0700

    fixed graphql auth request being undefined

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -7,14 +7,20 @@ import {
 } from '@nestjs/common';
 import { SupabaseClient } from '@supabase/supabase-js';
 import { Request } from 'express';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(@Inject('SUPABASE_CLIENT') private supabase: SupabaseClient) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request: Request = context.switchToHttp().getRequest();
-    console.log(request);
+    let request: Request;
+    if (context.getType<GqlContextType>() === 'graphql') {
+      const ctx = GqlExecutionContext.create(context);
+      request = ctx.getContext().req;
+    } else {
+      request = context.switchToHttp().getRequest();
+    }
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException('User not logged in!');
